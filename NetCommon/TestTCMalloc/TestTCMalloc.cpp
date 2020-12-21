093313419cf2ee511715be910ca74279b3efeaf9
RickerJee <76201872+RickerJee@users.noreply.github.com>
2020-12-21 11:20:43 +0800

    Add tcmalloc

---
+++ NetCommon/TestTCMalloc/TestTCMalloc.cpp
@@ -0,0 +1,37 @@
+// TestTCMalloc.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < 100;++i)
+    {
+        int* a = new int[128];
+
+        delete[] a;
+
+        float* b = new float();
+
+        delete b;
+
+        int* c = new int[1024 * 10];
+
+        delete[]c;
+
+        int* d = new int[1024 * 1024 * 3];
+
+        delete[] d;
+    }
+
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+
+    std::cout << "Timer: "<<duration.count()/1000.0f <<" ms";
+}
