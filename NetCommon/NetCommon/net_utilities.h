52ab40087296232d683e0447f37ea147a808af01
RickerJee <76201872+RickerJee@users.noreply.github.com>
2020-12-22 16:50:33 +0800

    Create net_utilities.h

---
+++ NetCommon/NetCommon/net_utilities.h
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+void ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+		return !std::isspace(ch);
+	}));
+}
+
+// trim from end (in place)
+void rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+		return !std::isspace(ch);
+	}).base(), s.end());
+}
+
+// trim from both ends (in place)
+void trim(std::string &s) {
+	ltrim(s);
+	rtrim(s);
+}
+void string_split(const string& s, vector<string>& tokens, const string& delimiters = ",")
+{
+	tokens.clear();
+	string::size_type lastPos = s.find_first_not_of(delimiters, 0);
+	string::size_type pos = s.find_first_of(delimiters, lastPos);
+	while (string::npos != pos || string::npos != lastPos) {
+		string szSub = s.substr(lastPos, pos - lastPos);
+		trim(szSub);
+		tokens.push_back(std::move(szSub));//use emplace_back after C++11
+		lastPos = s.find_first_not_of(delimiters, pos);
+		pos = s.find_first_of(delimiters, lastPos);
+	}
+}
